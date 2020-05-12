00e16891ea5d96409e8f4e2402f3abd977342b85
Joseph Kim <josephkim570@gmail.com>
2020-05-12 19:32:59 -0400

    Initially went for the Netlify route with zero luck, firebase seems to be working right now - need to adjust to make sure routing is behaving correctly

---
+++ build/precache-manifest.938d1fe933b88a1fdb268909f69717ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d6095f3dfffefa34a11aa72dbbbdcf4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3385d2b88c87d98bcba2",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "06db20f5ddb1e1a03b38",
+    "url": "/static/css/main.80bafdea.chunk.css"
+  },
+  {
+    "revision": "3385d2b88c87d98bcba2",
+    "url": "/static/js/2.0198471f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.0198471f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06db20f5ddb1e1a03b38",
+    "url": "/static/js/main.6f61568e.chunk.js"
+  },
+  {
+    "revision": "21214961ea4bebe0e493",
+    "url": "/static/js/runtime-main.ce039328.js"
+  }
+]);
